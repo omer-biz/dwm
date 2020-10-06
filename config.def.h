e38bf4093622d650e7043aac002bd179838e5fe8
omer <omerabdi@protonmail.com>
2020-10-06 21:32:16 +0300

    colors

--- config.def.h
+++ config.def.h
@@ -16,12 +16,12 @@ static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#ebdbb2";
+static const char col_gray4[]       = "#fbf1c7";
 static const char col_cyan[]        = "#427b58";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  "#8ec07c"},
+	[SchemeSel]  = { col_gray4, col_cyan,  "#fbf1c7" },
 	[SchemeHid]  = { col_cyan,  col_gray1, col_cyan  },
 };
 
