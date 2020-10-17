1ae5e7d2f40dbe8ebeaf9cbd172b30dfa3fc4a39
omer <omerabdi@protonmail.com>
2020-10-17 11:47:59 +0300

    Mod+Return was not working, just fixed that.

--- config.def.h
+++ config.def.h
@@ -115,7 +115,6 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_l,      setcfact,       {.f = -0.25} },
 	{ MODKEY|ShiftMask,             XK_o,      setcfact,       {.f =  0.00} },
  	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
