d965e3b39b209048cf39bc0e9df6b480c629f486
omer <omerabdi@protonmail.com>
2020-11-10 21:13:07 +0300

    Centered patch

--- config.def.h
+++ config.def.h
@@ -42,15 +42,15 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     	instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    	NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox", 	NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "St",      	NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ "Alacritty",  NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ "mpv",      	"gl",     NULL,           0,         0,          0,           0,        -1 },
-	{ NULL,      	NULL,     "n",            0,         0,          0,           0,        -1 },
-	{ NULL,      	NULL,     "pulsemixer",   0,         1,          0,           0,        -1 },
-	{ NULL,      	NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	/* class     	instance  title           tags mask 	iscentered  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",    	NULL,     NULL,           0,         	0,			1,          0,           0,        -1 },
+	{ "Firefox", 	NULL,     NULL,           1 << 8,    	0,			0,          0,          -1,        -1 },
+	{ "St",      	NULL,     NULL,           0,         	0,			0,          1,           0,        -1 },
+	{ "Alacritty",  NULL,     NULL,           0,         	0,			0,          1,           0,        -1 },
+	{ "mpv",      	"gl",     NULL,           0,         	0,			0,          0,           0,        -1 },
+	{ NULL,      	NULL,     "n",            0,         	0,			0,          0,           0,        -1 },
+	{ NULL,      	NULL,     "pulsemixer",   0,         	1,			1,          0,           0,        -1 },
+	{ NULL,      	NULL,     "Event Tester", 0,         	0,			0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
--- dwm.c
+++ dwm.c
@@ -119,7 +119,7 @@ struct Client {
 	int basew, baseh, incw, inch, maxw, maxh, minw, minh;
 	int bw, oldbw;
 	unsigned int tags;
-	int isfixed, isfloating, isurgent, neverfocus, oldstate, isfullscreen, isterminal, noswallow;
+	int isfixed, iscentered, isfloating, isurgent, neverfocus, oldstate, isfullscreen, isterminal, noswallow;
 	pid_t pid;
 	Client *next;
 	Client *snext;
@@ -175,6 +175,7 @@ typedef struct {
 	const char *instance;
 	const char *title;
 	unsigned int tags;
+	int iscentered;
 	int isfloating;
 	int isterminal;
 	int noswallow;
@@ -413,6 +414,7 @@ applyrules(Client *c)
 	XClassHint ch = { NULL, NULL };
 
 	/* rule matching */
+	c->iscentered = 0;
 	c->isfloating = 0;
 	c->tags = 0;
 	XGetClassHint(dpy, c->win, &ch);
@@ -425,6 +427,7 @@ applyrules(Client *c)
 		&& (!r->class || strstr(class, r->class))
 		&& (!r->instance || strstr(instance, r->instance)))
 		{
+			c->iscentered = r->iscentered;
 			c->isterminal = r->isterminal;
 			c->noswallow  = r->noswallow;
 			c->isfloating = r->isfloating;
@@ -1456,6 +1459,10 @@ manage(Window w, XWindowAttributes *wa)
 	updatewindowtype(c);
 	updatesizehints(c);
 	updatewmhints(c);
+	if (c->iscentered) {
+		c->x = c->mon->mx + (c->mon->mw - WIDTH(c)) / 2;
+		c->y = c->mon->my + (c->mon->mh - HEIGHT(c)) / 2;
+	}
 	XSelectInput(dpy, w, EnterWindowMask|FocusChangeMask|PropertyChangeMask|StructureNotifyMask);
 	grabbuttons(c, 0);
 	if (!c->isfloating)
@@ -2906,8 +2913,10 @@ updatewindowtype(Client *c)
 
 	if (state == netatom[NetWMFullscreen])
 		setfullscreen(c, 1);
-	if (wtype == netatom[NetWMWindowTypeDialog])
+	if (wtype == netatom[NetWMWindowTypeDialog]) {
+		c->iscentered = 1;
 		c->isfloating = 1;
+	}
 }
 
 void
