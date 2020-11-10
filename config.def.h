7daaafedaf24bbd44635920a513414ad568919c6
omer <omerabdi@protonmail.com>
2020-11-10 20:55:43 +0300

    Boxdraw patch

--- config.def.h
+++ config.def.h
@@ -14,7 +14,7 @@ static const unsigned int gappov    = 7;       /* vert outer gap between windows
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "UbuntuMono:size=11" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
@@ -30,8 +30,7 @@ static const char *colors[][3]      = {
 };
 
 static const char *const autostart[] = {
-	// "xautolock", "-time", "1", "-locker", "slock", NULL,
-	"locker.sh", NULL,
+	"sh", "-c", "\"$HOME/.local/scripts/locker.sh\"", NULL,
 	NULL /* terminate */
 };
 
@@ -43,12 +42,15 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,      "mpv",    NULL,           0,         1,          0,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	/* class     	instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",    	NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "Firefox", 	NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "St",      	NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "Alacritty",  NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "mpv",      	"gl",     NULL,           0,         0,          0,           0,        -1 },
+	{ NULL,      	NULL,     "n",            0,         0,          0,           0,        -1 },
+	{ NULL,      	NULL,     "pulsemixer",   0,         1,          0,           0,        -1 },
+	{ NULL,      	NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
@@ -82,7 +84,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", "-e", "tmux", "-f", "/home/omer/.config/tmux/tmux_scratch.conf", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
