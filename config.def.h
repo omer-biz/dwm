c6eb9d3fcfe346a1ac8d797ef140350304013a6a
omer <omerabdi@protonmail.com>
2020-11-28 16:05:06 +0300

    Notitle patch

--- config.def.h
+++ config.def.h
@@ -23,9 +23,14 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#fbf1c7";
 static const char col_cyan[]        = "#427b58";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_gray4 },//"#8ec07c" },
+	/*                         fg         bg         border   */
+	[SchemeNorm]        = { col_gray4, col_gray1, col_gray2 },
+	[SchemeSel]         = { col_gray4, col_cyan,  col_gray4 },//"#8ec07c" },
+	[SchemeStatus]      = { col_gray4, col_gray1,  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
+	[SchemeTagsSel]     = { col_gray4, col_cyan,  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
+    [SchemeTagsNorm]    = { col_gray4, col_gray1,  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
+    [SchemeInfoSel]     = { col_gray4, col_gray1,  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
+    [SchemeInfoNorm]    = { col_gray4, col_gray1,  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
 static const char *const autostart[] = {
